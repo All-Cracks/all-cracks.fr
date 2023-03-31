9dd248dc2378d7a5c2254d1143193848fbb1b2d7
Konixy <konixy.p@gmail.com>
2023-03-31 22:17:04 +0200

    Update

---
+++ lib/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  discordInvite: 'https://discord.all-cracks.fr',
+  nightCorpInvite: 'https://discord.gg/SMsMSnrXMq',
+  ownerName: 'Konixy#1905',
+  titleSufix: ' • All-Cracks.fr',
+};
+
+export default config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import ContentLoader from 'react-content-loader';
 // import Palette from 'react-palette';
 import Head from 'next/head';
 import { useGames } from '@/lib/gameContext';
+import Palette from 'react-palette';
 
 export default function Home() {
   const { games, loadGames, state } = useGames();
@@ -26,45 +27,42 @@ export default function Home() {
           <h1 className="text-2xl font-semibold text-slate-800">Nos dernières sorties :</h1>
           <div className="mt-10 flex flex-col items-center justify-evenly md:flex-row lg:mx-10">
             {state.success && games ? (
-              [0, 1].map(
-                (i) => (
-                  // <Palette key={e._id} src={e.coverUrl as string}>
-                  //   {({ data, loading, error }) => {
-                  //     console.log(data, loading, error);
-                  //     return (
-                  <div className="flex flex-row rounded-lg" key={games[i]._id} id={games[i]._id + '-container'}>
-                    {/* style={{ backgroundColor: data.darkVibrant || '#334155' }}> */}
-                    <div className="block w-[180px]">
-                      <Link href={`/games/${games[i]._id}`} className="w-[180px]">
-                        <Tilt max={12.5} speed={400} scale={1.07} reverse={true}>
-                          {/* eslint-disable-next-line @next/next/no-img-element */}
-                          <img
-                            src={games[i].coverUrl || ''}
-                            alt={games[i].name}
-                            width="180px"
-                            height="240px"
-                            className="game-card-img tilt relative h-[240px] w-[180px] rounded-lg"
-                            data-tilt
-                            id={games[i]._id + '-image'}
-                          />
-                        </Tilt>
-                      </Link>
-                    </div>
-                    <div className="relative flex flex-col whitespace-normal px-10 py-6 md:justify-between">
-                      <Link href={`/games/${games[i]._id}`} className="text-lg">
-                        {games[i].name}
-                      </Link>
-                      <p className="text-description text-sm">{games[i].description}</p>
-                      <Link href={`/games/${games[i]._id}`} className="rounded-lg bg-slate-200 px-2 py-1 text-black">
-                        Découvrir <i className="fa-solid fa-arrow-right"></i>
-                      </Link>
-                    </div>
-                  </div>
-                ),
-                //     );
-                //   }}
-                // </Palette>
-              )
+              [0, 1].map((i) => (
+                <Palette key={games[i]._id} src={games[i].coverUrl as string}>
+                  {({ data, loading, error }) => {
+                    console.log(data, loading, error);
+                    return (
+                      <div className="flex flex-row rounded-lg" id={games[i]._id + '-container'}>
+                        <div className="block w-[180px]" style={{ backgroundColor: data.vibrant || '#334155' }}>
+                          <Link href={`/games/${games[i]._id}`} className="w-[180px]">
+                            <Tilt max={12.5} speed={400} scale={1.07} reverse={true}>
+                              {/* eslint-disable-next-line @next/next/no-img-element */}
+                              <img
+                                src={games[i].coverUrl || ''}
+                                alt={games[i].name}
+                                width="180px"
+                                height="240px"
+                                className="game-card-img tilt relative h-[240px] w-[180px] rounded-lg"
+                                data-tilt
+                                id={games[i]._id + '-image'}
+                              />
+                            </Tilt>
+                          </Link>
+                        </div>
+                        <div className="relative flex flex-col whitespace-normal px-10 py-6 md:justify-between">
+                          <Link href={`/games/${games[i]._id}`} className="text-lg">
+                            {games[i].name}
+                          </Link>
+                          <p className="text-description text-sm">{games[i].description}</p>
+                          <Link href={`/games/${games[i]._id}`} className="rounded-lg bg-slate-200 px-2 py-1 text-black">
+                            Découvrir <i className="fa-solid fa-arrow-right"></i>
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  }}
+                </Palette>
+              ))
             ) : state.loading ? (
               [1, 2].map((e) => (
                 <div className="flex h-[240px] flex-row rounded-lg bg-slate-600/50" key={e}>
